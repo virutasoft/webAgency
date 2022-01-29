e142a9b19115ac95e48c92672af6d7d44bd05def
Cristhian <virutasoft@gmail.com>
2022-01-29 00:29:32 -0500

    FAQ section finished

--- js/main.js
+++ js/main.js
@@ -17,4 +17,11 @@ $(document).ready(function(){
         }
     });
 
+    $('.accordion-header').click(function(){
+        $('.accordion .accordion-body').slideUp();
+        $(this).next('.accordion-body').slideDown();
+        $('.accordion .accordion-header span').text('+');
+        $(this).children('span').text('-');
+    });
+
 });
